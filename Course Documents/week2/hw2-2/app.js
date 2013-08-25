962418ce0b395c6180eb412271abd422fa70cecb
Salvatore D'Agositno <toto19@gmail.com>
2013-08-24 23:32:10 -0400

    got script to return correct data set. now need to update document

--- Course Documents/week2/hw2-2/app.js
+++ Course Documents/week2/hw2-2/app.js
@@ -3,10 +3,17 @@ var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://localhost:27017/m101', function(err, db) {
     if(err) throw err;
 
+    var sort    = [["State",1],["Temperature",-1]];
     var query   = {};
-    var options = {};
-    var sort    = {"State":1,"Temperature":-1}
-    var cursor  = db.collection('hw2_1').find(query,options).sort();
+    var options = {
+        // 'limit' : 4,
+        'sort'  : sort
+    };
+
+    var weather = db.collection('hw2_1');
+    var cursor  = weather.find(query,{},options);
+
+    var currentState = "";
 
     cursor.each(function(err, doc) {
         if(err) throw err;
@@ -14,11 +21,21 @@ MongoClient.connect('mongodb://localhost:27017/m101', function(err, db) {
         if (!doc) {
             console.dir("No document found");
             return db.close();
+        } else if(doc == null) {
+            console.dir("Null document found");
+            return db.close();
         }
 
-        // TODO: Find Highest Temp for State
-        console.dir(doc);
+        // Are we at a new state?
+        if (currentState != doc.State) {
+            currentState = doc.State;
+            console.dir(doc);
 
-        return db.close();
+            // update this document with "month_high" : true
+            query['_id']      = doc['_id'];
+            doc["month_high"] = true;
+            // db.collections('hw2_1').update();
+        }
     });
+    // return db.close();
 });
